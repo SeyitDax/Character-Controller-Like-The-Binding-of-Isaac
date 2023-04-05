4aac1de63ee4e1a5858cb149f4dd26f8fb66ee7e
Seyit Ahmet Demir <129950748+SeyitDax@users.noreply.github.com>
2023-04-05 16:35:59 +0300

    Add files via upload

---
+++ Enemy_Monster_Damage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam;
+
+public static class Enemy_Monster_Damage : MonoBehaviour
+{
+	public int damage;
+	public PlayerHealth playerhealth;
+	
+	// bundan önceki player health scriptine bağlanması gerekiyor ki damahe fonksiyonu işe yarasın
+	
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.tag == "Player")
+		{
+			playerhealth.TakeDamage(damage)
+			
+			//Bu script düşmana bağlı olacak ve çalışması için karaktere "Player" tag ini koymanız gerek
+		}
+	}
+}
