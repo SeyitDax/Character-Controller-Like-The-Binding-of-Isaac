08d57641aa84d608040910b9e0e3227d3ce476a9
Seyit Ahmet Demir <129950748+SeyitDax@users.noreply.github.com>
2023-04-05 15:41:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Program;
+
+public static class Program : MonoBehaviour
+{         
+	Rigidbody2D rigid2d;
+	Vector3 movement;
+	private float speed=3f;
+	
+    public void Awake()
+	{
+		rigid2d = GetComponent<Rigidbody2D>();
+		movement = new Vector3();
+		
+	}
+	
+	void Update()
+	{
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+		
+		rigid2d.velocity = movement*speed*Time.deltaTime;
+	}
+}
