27039ab358c712df23db6b58361079d9ac4ad3ff
Seyit Ahmet Demir <129950748+SeyitDax@users.noreply.github.com>
2023-04-06 15:00:18 +0300

    Add files via upload
    Bunu düşmana koyun

---
+++ Düşman.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnemyScript;
+
+public class Düşman : MonoBehaviour
+{
+	public Animator animator;
+	public int maxHealth = 100;
+	int currentHealth;
+	
+	void Start()
+	{
+		currentHealth = maxHealth;
+	}
+	
+	public void TakeDamage(int zarar)
+	{
+		currentHealth -= zarar;
+		
+		animator.SetTrigger("Vurul");
+		
+		if(currentHealth <= 0)
+		{
+			Öl();
+		}
+	}
+	
+	void Öl()
+	{
+		animator.SetBool("Ölü", true);
+		GetComponent<Collider2D>().enabled = false;
+		this.enabled = false;
+	}
+	
+}
